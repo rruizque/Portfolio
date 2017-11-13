18d8000654fb2169bfe1ec6cc01df6a020a861ff
rruizque <rodrigorq@yahoo.com>
2017-11-13 12:18:48 -0800

    adding all other files

---
+++ myScript.js
@@ -0,0 +1,12 @@
+	function myHeader()
+		{
+			var x = document.getElementById("myTopnav");
+			if(x.className === "topnav")
+			{
+				x.className += " responsive";
+			}
+			else
+			{
+				x.className = "topnav";
+			}
+		}
